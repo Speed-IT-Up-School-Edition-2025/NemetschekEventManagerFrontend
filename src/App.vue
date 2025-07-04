f32318b284bba11fb574897fdf10a7e9b847a316
Alexander Shestakov <alexandurshestakov@gmail.com>
2025-07-04 15:57:17 +0300

    created a navbar with links for the event pages and login/register TODO: add links for event organizer pages when we get authStore

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
 	<header>
-		<div class="wrapper">
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-			</nav>
-		</div>
+		<NavBar />
 	</header>
 
-	<RouterView />
+	<main>
+		<RouterView />
+	</main>
 </template>
