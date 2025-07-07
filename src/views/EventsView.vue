853dbb750c2727393d03e495e4241541a9bc7919
KoaliM <mihaelahristova071@gmail.com>
2025-07-07 14:52:52 +0300

    changes to layout of event page

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -6,7 +6,8 @@ const events = getEvents();
 </script>
 
 <template>
-	<div class="pl-5 pt-10 pr-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 overflow-y-auto">
-		<CardComponent v-for="event in events" :event="event" :key="event.id" />
+	<div
+		class="pl-5 pt-10 pr-5 grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-6 justify-items-center overflow-y-auto">
+		<CardComponent class="max-w-md" v-for="event in events" :event="event" :key="event.id" />
 	</div>
 </template>
