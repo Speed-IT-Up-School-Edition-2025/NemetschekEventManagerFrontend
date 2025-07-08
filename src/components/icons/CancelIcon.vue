90131d4418987456290058eb2d58488e88894cc3
Yavor Momchev <yavormomchev2009@abv.bg>
2025-07-08 13:31:29 +0300

    Added cancel icon

---
+++ src/components/icons/CancelIcon.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="w-6 h-6 rounded-full bg-yellow flex items-center justify-center flex-shrink-0 mt-1">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			height="24px"
+			viewBox="0 -960 960 960"
+			width="24px"
+			fill="#e3e3e3">
+			<path
+				d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+		</svg>
+	</div>
+</template>
