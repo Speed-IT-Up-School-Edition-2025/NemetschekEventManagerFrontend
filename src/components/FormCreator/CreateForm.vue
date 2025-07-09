9fc3d2305645e82dcb765f1a6695e90c75c66c7e
Yavor Momchev <yavormomchev2009@abv.bg>
2025-07-09 14:44:22 +0300

    Translated Form creator to bg

--- src/components/FormCreator/CreateForm.vue
+++ src/components/FormCreator/CreateForm.vue
@@ -17,13 +17,13 @@ const addField = (fieldType: "text" | "checkbox" | "radio") => {
 	const newField: FormField = {
 		id: (fields.value[fields.value.length - 1]?.id ?? 0) + 1,
 		type: fieldType,
-		name: "New field",
+		name: "Ново поле",
 		required: false,
 		options: [],
 	};
 
 	if (fieldType === "checkbox" || fieldType === "radio") {
-		newField.options.push("Option 1");
+		newField.options.push("Опция 1");
 	}
 
 	fields.value.push(newField);
@@ -33,7 +33,7 @@ const addField = (fieldType: "text" | "checkbox" | "radio") => {
 <template>
 	<div class="p-6 bg-dark-grey shadow-lg rounded-lg max-w-4xl mx-auto my-8">
 		<form class="space-y-6" @submit.prevent="$emit('submit-form', fields)">
-			<h1 class="text-2xl font-semibold text-white text-center">Form Creator</h1>
+			<h1 class="text-2xl font-semibold text-white text-center">Конфигурация на формат</h1>
 			<div
 				v-for="(field, index) in fields"
 				:key="field.id"
@@ -58,12 +58,12 @@ const addField = (fieldType: "text" | "checkbox" | "radio") => {
 							class="sr-only peer" />
 						<div
 							class="w-11 h-6 bg-grey-400 rounded-full peer peer-focus:ring-3 peer-focus:ring-yellow peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-grey-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-yellow"></div>
-						<span class="ml-3 text-sm font-medium text-white-900">Required</span>
+						<span class="ml-3 text-sm font-medium text-white-900">Задължително</span>
 					</label>
 					<button
 						class="border border-red text-red hover:bg-red hover:text-white focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-4 py-1.5 text-center transition-colors"
 						@click="fields.splice(index, 1)">
-						Remove field
+						Премахни поле
 					</button>
 				</div>
 
@@ -72,7 +72,7 @@ const addField = (fieldType: "text" | "checkbox" | "radio") => {
 						type="text"
 						disabled
 						class="text-white mt-1 block w-full px-4 py-2 border-b border-outline bg-dark-grey rounded-t-md shadow-sm focus:border-primary focus:ring-yellow sm:text-base transition duration-150 ease-in-out"
-						:placeholder="`Enter ${field.name.toLowerCase()}`" />
+						:placeholder="`Въведи ${field.name.toLowerCase()}`" />
 				</div>
 
 				<div
@@ -101,8 +101,8 @@ const addField = (fieldType: "text" | "checkbox" | "radio") => {
 					</div>
 					<button
 						class="mt-2 text-yellow-400 hover:bg-yellow-400/20 text-sm font-medium rounded-md px-3 py-1.5 transition-colors"
-						@click.prevent="field.options.push(`Option ${field.options.length + 1}`)">
-						Add Option
+						@click.prevent="field.options.push(`Oпция ${field.options.length + 1}`)">
+						Добави опция
 					</button>
 				</div>
 			</div>
