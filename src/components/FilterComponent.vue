6465e9cbe833fd3aafdb5fdb03db334495eb364b
KoaliM <mihaelahristova071@gmail.com>
2025-07-10 10:56:33 +0300

    editting search/filter/sort in events view

---
+++ src/components/FilterComponent.vue
@@ -0,0 +1,254 @@
+<script setup lang="ts">
+import SearchIcon from "@/components/icons/SearchIcon.vue";
+import { getEvents } from "@/services/eventsService";
+import { ref, watch, onMounted } from "vue";
+import { TransitionRoot, TransitionChild } from "@headlessui/vue";
+import DatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import ChevronDownIcon from "@/components/icons/ChevronDownIcon.vue";
+import ChevronUp from "@/components/icons/ChevronUp.vue";
+import FilterIcon from "./icons/FilterIcon.vue";
+import SortIcon from "./icons/SortIcon.vue";
+
+const search = ref("");
+const events = getEvents();
+const sortedEvents = ref();
+const filtersOpen = ref(false);
+const date = ref<[Date, Date] | null>(null);
+const activeEvent = ref(false);
+const sortOpened = ref(false);
+
+onMounted(() => {
+	const startDate = new Date();
+	const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
+	date.value = [startDate, endDate];
+});
+
+const textInputOptions = {
+	format: "dd.MM.yyyy",
+};
+async function sortBy(text: string): Promise<void> {
+	await new Promise(resolve => setTimeout(resolve, 100));
+	sortedEvents.value = events.filter(
+		currentEvent => currentEvent.title.includes(text) || currentEvent.description.includes(text)
+	);
+}
+watch(search, async changedValue => {
+	if (changedValue) {
+		await sortBy(changedValue);
+	} else {
+		sortedEvents.value = null;
+	}
+});
+async function filterBy(): Promise<void> {
+	/*await new Promise(resolve => setTimeout(resolve, 100));
+	const info = search.value.split(" ");
+	sortedEvents.value = events.filter(
+		currentEvent => {
+			info.some(sub => currentEvent.title.toLowerCase().includes(sub) || currentEvent.description.toLowerCase().includes(sub))
+			const date = 
+			const yesActive = currentEvent.signUpDeadline > 
+		}
+	);*/
+}
+</script>
+<template>
+	<div class="hidden md:flex justify-between items-start">
+		<span class="flex items-center">
+			<div class="relative inline-block text-left sm:gap-0 md:gap-2 pl-10 pt-10">
+				<button
+					type="button"
+					@click="filtersOpen = !filtersOpen"
+					class="flex justify-between px-5 items-center h-10 w-40 border rounded border-yellow-500 text-yellow transition delay-100 duration-300 hover:ease-in-out hover:border-transparent hover:text-white/100 hover:bg-yellow">
+					<span>Филтрирай</span>
+					<ChevronDownIcon v-if="filtersOpen" />
+					<ChevronUp v-else />
+				</button>
+				<TransitionRoot as="template" :show="filtersOpen">
+					<TransitionChild
+						as="template"
+						enter="transition-opacity ease-in-out duration-300 transform"
+						enter-from="opacity-0 -translate-y-5"
+						enter-to="opacity-100 translate-y-0"
+						leave="transition-opacity ease-in duration-300 transform"
+						leave-from="opacity-100 translate-y-0"
+						leave-to="opacity-0 -translate-y-3">
+						<div
+							class="absolute mt-1 flex flex-col h-30 w-90 rounded origin-top-left border-2 border-yellow bg-dark-grey shadow-lg z-50">
+							<div
+								class="flex flex-row gap-5 pt-5 pl-6 pr-4 text-yellow space-y-5 rounded-md">
+								<span class="text-white text-lg self-center">Период: </span>
+								<DatePicker
+									class="w-md"
+									v-model="date"
+									placeholder="Период"
+									range
+									:text-input="textInputOptions"
+									:enable-time-picker="false"
+									:inputClass="'filterCalendar'"
+									position="center"
+									circle
+									lang="bg"
+									dark />
+							</div>
+							<div class="flex flex-row justify-between items-center pl-6 pr-3">
+								<span class="flex gap-4">
+									<input
+										type="checkbox"
+										name="active"
+										id="active"
+										v-model="activeEvent"
+										class="w-5 h-5" />
+									<label for="active" class="text-lg self-center"
+										>Активно събитие</label
+									>
+								</span>
+								<button
+									@click="filtersOpen = false && filterBy"
+									class="border rounded-2xl p-1 bg-yellow text-black hover:shadow-xl transition ease-in-out duration-200 delay-100 shadow-yellow/30">
+									Запази
+								</button>
+							</div>
+						</div>
+					</TransitionChild>
+				</TransitionRoot>
+			</div>
+			<div class="relative inline-block text-left pl-5 pt-10">
+				<button
+					type="button"
+					@click="sortOpened = !sortOpened"
+					class="px-5 text-yellow transition delay-100 duration-300 hover:ease-in-out hover:border-transparent hover:text-white/100">
+					<SortIcon class="w-8 h-8" />
+				</button>
+				<TransitionRoot as="template" :show="sortOpened">
+					<TransitionChild
+						as="template"
+						enter="transition-opacity ease-in-out duration-200 transform"
+						enter-from="opacity-0 -translate-y-5"
+						enter-to="opacity-100 translate-y-0"
+						leave="transition-opacity ease-in duration-200 transform"
+						leave-from="opacity-100 translate-y-0"
+						leave-to="opacity-0 -translate-y-3">
+						<div
+							class="absolute flex justify-evenly mt-3 flex-col h-30 w-40 rounded-2xl origin-top-left border-2 border-yellow bg-dark-grey shadow-lg z-50">
+							<button class="text-white hover:text-yellow">Най-нови</button>
+							<button class="text-white hover:text-yellow">Най-стари</button>
+							<button class="text-white hover:text-yellow">
+								Азбучен ред: А до Я
+							</button>
+							<button class="text-white hover:text-yellow">
+								Азбучен ред: Я до А
+							</button>
+						</div>
+					</TransitionChild>
+				</TransitionRoot>
+			</div>
+		</span>
+		<div class="flex items-center gap-4 pr-10 pt-10">
+			<input
+				v-model="search"
+				placeholder="Потърси..."
+				class="flex-1 max-w-100 h-10 px-3 border rounded focus:outline-none focus:border-white text-yellow" />
+			<SearchIcon class="w-10 h-10" />
+		</div>
+	</div>
+	<div class="md:hidden">
+		<div class="flex justify-between items-start">
+			<div class="flex items-center">
+				<div class="relative inline-block text-left pl-4 pt-10">
+					<button
+						type="button"
+						@click="filtersOpen = !filtersOpen"
+						class="flex justify-between px-5 items-center text-yellow transition delay-100 duration-300 hover:ease-in-out">
+						<FilterIcon />
+					</button>
+					<TransitionRoot as="template" :show="filtersOpen">
+						<TransitionChild
+							as="template"
+							enter="transition-opacity ease-in-out duration-200"
+							enter-from="opacity-0 -translate-y-5"
+							enter-to="opacity-100 translate-y-0"
+							leave="transition-opacity ease-in duration-200"
+							leave-from="opacity-100 translate-y-0"
+							leave-to="opacity-0 -translate-y-3">
+							<div
+								class="absolute mt-1 flex flex-1 flex-col h-30 w-90 rounded origin-top-left border-2 border-yellow bg-dark-grey shadow-lg z-50">
+								<div
+									class="flex flex-row gap-5 pt-5 pl-6 pr-4 text-yellow space-y-5 rounded-md">
+									<span class="text-white text-lg self-center">Период: </span>
+									<DatePicker
+										class="w-md"
+										v-model="date"
+										placeholder="Период"
+										range
+										:text-input="textInputOptions"
+										:enable-time-picker="false"
+										:inputClass="'filterCalendar'"
+										position="center"
+										circle
+										lang="bg"
+										dark />
+								</div>
+								<div class="flex flex-row justify-between items-center pl-6 pr-3">
+									<span class="flex gap-4">
+										<input
+											type="checkbox"
+											name="active"
+											id="active"
+											v-model="activeEvent"
+											class="w-5 h-5" />
+										<label for="active" class="text-lg self-center"
+											>Активно събитие</label
+										>
+									</span>
+									<button
+										@click="filtersOpen = false && filterBy"
+										class="border rounded-2xl p-1 bg-yellow text-black hover:shadow-xl transition ease-in-out duration-200 delay-100 shadow-yellow/30">
+										Запази
+									</button>
+								</div>
+							</div>
+						</TransitionChild>
+					</TransitionRoot>
+				</div>
+				<div class="relative inline-block text-left pt-10">
+					<button
+						type="button"
+						@click="sortOpened = !sortOpened"
+						class="px-5 text-yellow transition delay-100 duration-300 hover:ease-in-out hover:border-transparent hover:text-white/100">
+						<SortIcon class="w-8 h-8" />
+					</button>
+					<TransitionRoot as="template" :show="sortOpened">
+						<TransitionChild
+							as="template"
+							enter="transition-opacity ease-in-out duration-200 transform"
+							enter-from="opacity-0 -translate-y-5"
+							enter-to="opacity-100 translate-y-0"
+							leave="transition-opacity ease-in duration-200 transform"
+							leave-from="opacity-100 translate-y-0"
+							leave-to="opacity-0 -translate-y-3">
+							<div
+								class="absolute flex justify-evenly mt-3 flex-col h-30 w-40 rounded-2xl origin-top-left border-2 border-yellow bg-dark-grey shadow-lg z-50">
+								<button class="text-white hover:text-yellow">Най-нови</button>
+								<button class="text-white hover:text-yellow">Най-стари</button>
+								<button class="text-white hover:text-yellow">
+									Азбучен ред: А до Я
+								</button>
+								<button class="text-white hover:text-yellow">
+									Азбучен ред: Я до А
+								</button>
+							</div>
+						</TransitionChild>
+					</TransitionRoot>
+				</div>
+			</div>
+			<div class="flex items-center gap-4 pr-5 pt-10">
+				<input
+					v-model="search"
+					placeholder="Потърси..."
+					class="max-w-100 flex-1 h-10 px-3 border rounded focus:outline-none focus:border-white text-yellow md:w-auto md:h-10" />
+				<SearchIcon class="w-10 h-10" />
+			</div>
+		</div>
+	</div>
+</template>
