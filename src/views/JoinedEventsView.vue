3c74dc40ab8d455cdfd26e3f0bb0a8a7b54a500f
KoaliM <mihaelahristova071@gmail.com>
2025-07-10 11:03:00 +0300

    changes on events and joined events view(adding filter component)

--- src/views/JoinedEventsView.vue
+++ src/views/JoinedEventsView.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import CardComponent from "@/components/CardComponent.vue";
+import FilterComponent from "@/components/FilterComponent.vue";
 import { getJoinedEvents } from "@/services/eventsService";
 
 const joinedEvents = getJoinedEvents();
 </script>
 
 <template>
-	<div class="p-6 flex flex-col gap-6">
-		<h2 class="text-3xl md:text-4xl px-6 font-bold text-yellow">
-			Присъединени събития
-		</h2>
-
+	<div class="pt-6 flex flex-col gap-6">
+		<h2 class="text-3xl md:text-4xl px-6 font-bold text-yellow">Присъединени събития</h2>
+		<FilterComponent />
 		<div
 			class="p-6 grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-10 justify-items-center overflow-y-auto">
 			<CardComponent
